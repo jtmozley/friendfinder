8b44e83c497e7c6cd91857059fc89652807d94bc
Justin Mozley <jtmozley@gmail.com>
2019-06-01 15:45:49 -0500

    added file routes to the htmlRoutes.js. Began adding basic profiles to the frieneds.js file. html and styling done for home and survey pages

---
+++ app/data/friends.js
@@ -0,0 +1,172 @@
+const friends = [
+    {
+        "name": "John",
+        "photo":"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "scores": [
+            3,
+            5,
+            1,
+            5,
+            4,
+            2,
+            1,
+            1,
+            3,
+            2
+        ]
+
+    },
+    {
+        "name": "Samantha",
+        "photo": "https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "scores": [
+            4,
+            5,
+            2,
+            5,
+            1,
+            2,
+            3,
+            1,
+            4,
+            2
+        ]
+
+    },
+    {
+        "name": "Julia",
+        "photo": "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "scores": [
+            3,
+            1,
+            1,
+            1,
+            4,
+            4,
+            1,
+            2,
+            3,
+            5
+        ]
+
+    },
+    {
+        "name": "Todd",
+        "photo": "https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "scores": [
+            2,
+            5,
+            2,
+            5,
+            1,
+            2,
+            5,
+            1,
+            3,
+            2
+        ]
+
+    },
+    {
+        "name": "Anne",
+        "photo": "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "scores": [
+            3,
+            5,
+            1,
+            1,
+            4,
+            5,
+            1,
+            5,
+            3,
+            1
+        ]
+
+    },
+    {
+        "name":,
+        "photo":,
+        "scores": [
+            3,
+            5,
+            1,
+            5,
+            4,
+            2,
+            1,
+            1,
+            3,
+            2
+        ]
+
+    },
+    {
+        "name":,
+        "photo":,
+        "scores": [
+            3,
+            5,
+            1,
+            5,
+            4,
+            2,
+            1,
+            1,
+            3,
+            2
+        ]
+
+    },
+    {
+        "name":,
+        "photo":,
+        "scores": [
+            3,
+            5,
+            1,
+            5,
+            4,
+            2,
+            1,
+            1,
+            3,
+            2
+        ]
+
+    },
+    {
+        "name":,
+        "photo":,
+        "scores": [
+            3,
+            5,
+            1,
+            5,
+            4,
+            2,
+            1,
+            1,
+            3,
+            2
+        ]
+
+    },
+    {
+        "name":,
+        "photo":,
+        "scores": [
+            3,
+            5,
+            1,
+            5,
+            4,
+            2,
+            1,
+            1,
+            3,
+            2
+        ]
+
+    }
+]
