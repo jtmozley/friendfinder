cdfca4e28020bd0463595616d7b71fbfb17c80f4
Justin Mozley <jtmozley@gmail.com>
2019-06-02 16:49:51 -0500

    setup working server and linked htmlroutes document. routes to both html docs working properly. setup GET in apiRoutes to bring in data from friends.js.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,172 +1,64 @@
 const friends = [
-    {
-        "name": "John",
-        "photo":"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "scores": [
-            3,
-            5,
-            1,
-            5,
-            4,
-            2,
-            1,
-            1,
-            3,
-            2
-        ]
+  {
+    name: "John",
+    photo:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    scores: [3, 5, 1, 5, 4, 2, 1, 1, 3, 2]
+  },
+  {
+    name: "Samantha",
+    photo:
+      "https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    scores: [4, 5, 2, 5, 1, 2, 3, 1, 4, 2]
+  },
+  {
+    name: "Julia",
+    photo:
+      "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    scores: [3, 1, 1, 1, 4, 4, 1, 2, 3, 5]
+  },
+  {
+    name: "Todd",
+    photo:
+      "https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    scores: [2, 5, 2, 5, 1, 2, 5, 1, 3, 2]
+  },
+  {
+    name: "Anne",
+    photo:
+      "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    scores: [3, 5, 1, 1, 4, 5, 1, 5, 3, 1]
+  },
+  {
+    name: "Brandon",
+    photo:
+      "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    scores: [1, 5, 3, 5, 3, 2, 2, 1, 5, 2]
+  },
+  {
+    name: "Stephen",
+    photo:
+      "https://images.pexels.com/photos/937481/pexels-photo-937481.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    scores: [3, 4, 1, 4, 4, 1, 1, 5, 3, 1]
+  },
+  {
+    name: "Veronica",
+    photo:
+      "https://images.pexels.com/photos/1082962/pexels-photo-1082962.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    scores: [4, 5, 2, 5, 5, 2, 2, 1, 4, 2]
+  },
+  {
+    name: "Zeke",
+    photo:
+      "https://images.pexels.com/photos/450271/pexels-photo-450271.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    scores: [3, 4, 1, 4, 4, 3, 1, 2, 3, 1]
+  },
+  {
+    name: "Jordan",
+    photo:
+      "https://images.pexels.com/photos/462680/pexels-photo-462680.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    scores: [1, 5, 5, 5, 2, 2, 5, 1, 3, 2]
+  }
+];
 
-    },
-    {
-        "name": "Samantha",
-        "photo": "https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "scores": [
-            4,
-            5,
-            2,
-            5,
-            1,
-            2,
-            3,
-            1,
-            4,
-            2
-        ]
-
-    },
-    {
-        "name": "Julia",
-        "photo": "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "scores": [
-            3,
-            1,
-            1,
-            1,
-            4,
-            4,
-            1,
-            2,
-            3,
-            5
-        ]
-
-    },
-    {
-        "name": "Todd",
-        "photo": "https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "scores": [
-            2,
-            5,
-            2,
-            5,
-            1,
-            2,
-            5,
-            1,
-            3,
-            2
-        ]
-
-    },
-    {
-        "name": "Anne",
-        "photo": "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "scores": [
-            3,
-            5,
-            1,
-            1,
-            4,
-            5,
-            1,
-            5,
-            3,
-            1
-        ]
-
-    },
-    {
-        "name": "Brandon",
-        "photo": "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "scores": [
-            1,
-            5,
-            3,
-            5,
-            3,
-            2,
-            2,
-            1,
-            5,
-            2
-        ]
-
-    },
-    {
-        "name": "Stephen",
-        "photo": "https://images.pexels.com/photos/937481/pexels-photo-937481.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "scores": [
-            3,
-            4,
-            1,
-            4,
-            4,
-            1,
-            1,
-            5,
-            3,
-            1
-        ]
-
-    },
-    {
-        "name": "Veronica",
-        "photo": "https://images.pexels.com/photos/1082962/pexels-photo-1082962.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "scores": [
-            4,
-            5,
-            2,
-            5,
-            5,
-            2,
-            2,
-            1,
-            4,
-            2
-        ]
-
-    },
-    {
-        "name": "Zeke",
-        "photo": "https://images.pexels.com/photos/450271/pexels-photo-450271.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "scores": [
-            3,
-            4,
-            1,
-            4,
-            4,
-            3,
-            1,
-            2,
-            3,
-            1
-        ]
-
-    },
-    {
-        "name": "Jordan",
-        "photo": "https://images.pexels.com/photos/462680/pexels-photo-462680.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "scores": [
-            1,
-            5,
-            5,
-            5,
-            2,
-            2,
-            5,
-            1,
-            3,
-            2
-        ]
-
-    }
-]
+module.exports = friends;
--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
+
 
 // Sets up the Express App
-var app = express();
-var PORT = 3000;
+const app = express();
+const PORT = 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+require("./app/routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
