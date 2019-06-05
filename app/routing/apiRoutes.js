fcaa2afb3de9dda20d71c7a1e1e26a7491887f8c
Justin Mozley <jtmozley@gmail.com>
2019-06-05 15:45:53 -0500

    apiroutes loop syntax started

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,4 +4,15 @@ module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
     return res.json(friends);
   });
+
+  app.post("/api/friends", function(req, res) {
+
+    const userScores = req.body.scores;
+
+    for(let i = 0; i < friends.length; i++){
+        for(let j =0; j < friends[i].scores.length; j++){
+            console.log(j)
+        }
+    }
+  });
 };
